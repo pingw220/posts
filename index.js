fcf5e0ceae8bd3921509abf27ec3cd5dd4d6ea9d
Peter Wang <pingw220@gmail.com>
2024-02-01 11:54:09 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function down1(element){
+    element.style.display="none";
+    let welcometwo=document.querySelector(".welcome2");
+    welcometwo.style.display="flex";
+}
+function down2(element){
+    element.style.display="none";
+    let welcomeone=document.querySelector(".welcome1");
+    welcomeone.style.display="flex";
+}
+function down3(element){
+    let welcomeone=document.querySelector(".list2");
+    welcomeone.style.display="block";
+}
+function down4(element){
+    let welcomeone=document.querySelector(".list2");
+    welcomeone.style.display="none";
+}
